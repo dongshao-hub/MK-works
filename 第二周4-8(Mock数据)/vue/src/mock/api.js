e1e9bd15e3785396720acea21ebe09c0aebe49e3
dongshao <dong158960@163.com>
2020-04-08 21:16:20 +0800

    first send

---
+++ 第二周4-8(Mock数据)/vue/src/mock/api.js
@@ -0,0 +1,35 @@
+import mock from "mockjs";
+// 这个一个获取URL参数的方法
+import { getParams } from "./utils";
+
+//Mock拦截并返回响应数据
+mock.mock(/\/api\/lists/, options => {
+  //获取url参数num
+  const num = getParams(options.url, "num");
+  console.log(num);
+
+  var datas = {
+    ['data |' + num]: [
+      {
+        "tid|+1": 1,
+        title: "@title(3,5)",
+        catalog: "index",
+        fav: /\d{3,5}/,
+        created: "@datetime('yyyy-MM-dd HH:mm:ss')",
+        "isEnd|0-1": 0,
+        "answer|0-100": 100,
+        user: {
+          avatar: "@image('80*80')",
+          name: "@cname",
+          "isVip|0-1": 0,
+          "level|0-99": 0,
+        }
+      }
+    ],
+    status: 0,
+    msg: "返回Mock数据",
+  }
+  return mock.mock(datas);
+});
+
+
